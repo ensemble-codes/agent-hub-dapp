73d1a45323448047a5985e81f9c9bb73f42eebef
Leon Prouger <leonprou@gmail.com>
2025-02-24 00:37:41 +0200

    Fixes for production build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
 "use client";
 import { useRouter, useSearchParams } from "next/navigation";
 import { AppHeader, HomeMain, SideMenu, TaskDetails } from "../components";
+import { Suspense } from 'react'
 
 export default function Home() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <AppHeader />
+      <InernalHome />
+    </Suspense>
+  );
+}
+
+function InernalHome() {
+
   const router = useRouter();
+
   const searchParams = useSearchParams();
   const selectedService = searchParams.get("service");
 
@@ -12,18 +24,16 @@ export default function Home() {
   };
 
   return (
-    <div>
-      <AppHeader />
       <div className="flex items-start gap-16 pt-16">
-        <SideMenu />
-        {!selectedService ? (
-          <HomeMain 
-            selectedService={(service) => handleServiceSelect(service)}
-          />
-        ) : (
-          <TaskDetails selectedService={selectedService} />
-        )}
-      </div>
+      <SideMenu />
+      {!selectedService ? (
+        <HomeMain 
+          selectedService={(service) => handleServiceSelect(service)}
+        />
+      ) : (
+        <TaskDetails selectedService={selectedService} />
+      )}
     </div>
-  );
+  )
+
 }
