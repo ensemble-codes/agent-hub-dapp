e2155f033660f2876605cd5779ad46d998007422
Leon Prouger <leonprou@gmail.com>
2025-03-10 12:49:15 +0200

    delete debug statements

--- src/components/homepage/taskdetails/select-step.tsx
+++ src/components/homepage/taskdetails/select-step.tsx
@@ -64,7 +64,6 @@ const SelectAgentStep: FC<SelectAgentStepProps> = ({ selectedAgent }) => {
     `,
     [selectedService, proposalId]
   );
-  debugger
   const { data, loading, error } = useQuery(GET_PROPOSALS);
 
   return loading ? (
