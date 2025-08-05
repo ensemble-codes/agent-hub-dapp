cc556d5cec4ecd77614b832f92bdc9a2715f45b0
Leon Prouger <leonprou@gmail.com>
2025-08-05 15:45:44 +0300

    use NEXT_PUBLIC_GRAPHQL_URL in all the places

--- codegen.ts
+++ codegen.ts
@@ -4,7 +4,7 @@ const config: CodegenConfig = {
     overwrite: true,
     generates: {
         "src/graphql/generated/ensemble.tsx": {
-            schema: 'https://api.goldsky.com/api/public/project_cmcnps2k01akp01uobifl4bby/subgraphs/ensemble-subgraph/0.0.1/gn',
+            schema: process.env.NEXT_PUBLIC_GRAPHQL_URL,
             documents: ['src/graphql/queries/agents.ts'],
             plugins: ['typescript', 'typescript-operations', 'typescript-react-apollo'],
         }
